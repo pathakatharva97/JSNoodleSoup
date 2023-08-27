295b038f3fa2e461df6d8e28edfa6dcd906d4af1
pathakatharva97 <143208306+pathakatharva97@users.noreply.github.com>
2023-08-27 13:40:56 +0000

    datatypes added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,3 +3,23 @@
 //alert(3 + 3)  // cannot use this syntax for alert in node. We are using nodeJS, not browser
 
 console.log(3 + 3);
+
+let name = "atharva"
+let age = 23
+let isLoggedIn = true
+
+/* Range of datatypes
+number => 2 to power 53
+bigint (bigger integer)
+string => ""
+boolean => true, false
+null => standalone value, can be given to an object
+undefined => 
+symbol => uniqueness
+
+object
+*/
+
+console.log(typeof "ATharva") //shows us the dataType
+console.log(typeof null)      //null is treated as an object
+console.log(typeof undefined) //undefined is not treated as an object
