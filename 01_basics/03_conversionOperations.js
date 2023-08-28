812c5fea768e80d1a2293d347865ce8e807b2849
pathakatharva97 <143208306+pathakatharva97@users.noreply.github.com>
2023-08-28 18:03:04 +0000

    new

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -13,11 +13,11 @@ let valueInNumber = Number(score)
 // true =>1 ; false=>0
 
 let isLoggedIn = 1
-console.log(isLoggedIn);
-console.log((Boolean), isLoggedIn);
+// console.log(isLoggedIn);
+// console.log((Boolean), isLoggedIn);
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn); //gives o/p true
+// console.log(booleanIsLoggedIn); //gives o/p true
 
 // 1 => true; 0 => false
 //"" => false
@@ -25,8 +25,46 @@ console.log(booleanIsLoggedIn); //gives o/p true
 
 let randomNumber = 30
 let stringNumber = String(randomNumber)
-console.log(randomNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
-console.log(randomNumber + 5); //35
-console.log(stringNumber + 5); //305  (as 30 is treated as a string and 5 as a suffix)
+// console.log(randomNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(randomNumber + 5); //35
+// console.log(stringNumber + 5); //305  (as 30 is treated as a string and 5 as a suffix)
+
+
+// ******************************* Operations *************************************
+
+let value = 5
+
+let negValue = -value
+// console.log(negValue);  //-5
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**2); //2 to power of 2 => 4
+// console.log(2/3); //0.66
+// console.log(2%3); //2 Rem
+
+let str1 = "hello"
+let str2 = " atharva"
+let str3 = str1 + str2
+// console.log(str3); //hello atharva
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122 Here first datatype is generally preferred for the entire
+// console.log(1 + 2 + "2"); //32 Here 1 and 2 are numbers and 2 is added as suffix string
+// console.log(+true); //1
+// console.log(true+); //error
+// console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2    //this is not a clean code
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter); //101 // Read prefix and postfix for JS from MDN
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
