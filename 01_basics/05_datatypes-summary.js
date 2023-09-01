c26147df5375b3431855e1c0886fbf6d3bcdfdf6
pathakatharva97 <143208306+pathakatharva97@users.noreply.github.com>
2023-09-01 09:45:22 +0000

    Stack and Heap Memory added

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -12,7 +12,7 @@ const anotherId = Symbol ('123')
 // console.log(id === anotherId); //false. Even if Symbol stores same data, they are different
 
 const bigNumber = 1646843216845613484n
-console.log(typeof bigNumber); //bigint
+// console.log(typeof bigNumber); //bigint
 
 
 //===================================Reference (Non - Primitive) =========================
@@ -29,8 +29,8 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myObj); //object
-console.log(typeof myFunction); //function-object
+// console.log(typeof myObj); //object
+// console.log(typeof myFunction); //function-object
 
 /*
     typeof Operator Results
@@ -48,3 +48,24 @@ Object(host and does not implement [[Call]])	=> Implementation-defined except ma
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 */
+
+// -------------------------------Memory-----------------------------------------
+
+//Stack (used by Primitive - we get a copy and operations on one copy wont change rest of the copies)
+
+// Heap (used by Non-Primitive - we get reference of that obj. So if we change any one entity in any Heap, it will be changed for all the other references)
+
+let myName = "Atharva"
+
+let user1 = {
+    email: "user.gooogle.com",
+    upi: "user@ybl"
+}
+
+let user2 = user1 
+
+user2.email = "anotherone@google.com"
+
+console.log(user1.email); //anotherone@google.com 
+console.log(user2.email); //anotherone@google.com
+//(Here the reference of user1 was passed so whenever we changed the email for user2, user1's email also got changed)
